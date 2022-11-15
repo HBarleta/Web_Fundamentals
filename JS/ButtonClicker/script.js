436769697e6997831fec1f212f60425d7cc0e32a
Harold Barleta <hbf20c@gmail.com>
2022-11-15 07:38:55 -0800

    added JS script

--- JS/ButtonClicker/script.js
+++ JS/ButtonClicker/script.js
@@ -2,3 +2,11 @@ function removeButton(element){
   element.remove();
 }
 
+function signIn(element){
+  if(element.innerText === "Login"){
+    element.innerText = "Logout";
+  } else {
+    element.innerText = "Login";
+  }
+  
+}
