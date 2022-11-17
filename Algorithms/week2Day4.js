68b7a4ae873b356e7871d996a48cbb0e77a1722f
Harold Barleta <hbf20c@gmail.com>
2022-11-17 15:22:01 -0800

    algos week2 day 4

---
+++ Algorithms/week2Day4.js
@@ -0,0 +1,10 @@
+
+function flatten(arr2d) {
+  for(let i = 0; i < arr2d.length; i++){
+    for(let x =0; x < arr2d[i].length; x++){
+      flat.push(arr2d[i][x]);
+    }
+}
+  
+var result = flatten( [ [2, 5, 8], [3, 6, 1], [5, 7, 7] ] );
+console.log(result); // we expect to get back [2, 5, 8, 3, 6, 1, 5, 7, 7]
