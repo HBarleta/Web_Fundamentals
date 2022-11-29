94698c3128e9f0dab43f0389d0914400fdc4daa9
Harold Barleta <hbf20c@gmail.com>
2022-11-29 13:51:40 -0800

    added algos

---
+++ Algorithms/PythonWeek1Day2.js
@@ -0,0 +1,7 @@
+const newWord = "Merry Chrysler";
+
+function reverse (word) {
+  return word.split('').reverse().join('');
+}
+
+console.log(reverse(newWord));
--- Algorithms/week2Day4.js
+++ Algorithms/week2Day4.js
@@ -4,6 +4,7 @@ function flatten(arr2d) {
     for(let x =0; x < arr2d[i].length; x++){
       flat.push(arr2d[i][x]);
     }
+  }
 }
   
 var result = flatten( [ [2, 5, 8], [3, 6, 1], [5, 7, 7] ] );
